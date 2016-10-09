ffcd23ddfa401c45be355cfd1725a9a57c41266e
zombiB <zombivert@gmail.com>
2016-10-09 22:01:31 +0100

    aracinema added

---
+++ plugin.video.smsm/resources/sites/aracinema_co.py
@@ -0,0 +1,351 @@
+﻿#-*- coding: utf-8 -*-
+#Venom.
+#zombi.(@geekzombi)
+from resources.lib.gui.hoster import cHosterGui
+from resources.lib.handler.hosterHandler import cHosterHandler
+from resources.lib.gui.gui import cGui
+from resources.lib.gui.guiElement import cGuiElement
+from resources.lib.handler.inputParameterHandler import cInputParameterHandler
+from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.config import cConfig
+from resources.lib.parser import cParser
+from resources.lib.util import cUtil
+import urllib2,urllib,re
+import unicodedata
+ 
+SITE_IDENTIFIER = 'aracinema_co'
+SITE_NAME = 'aracinema.co'
+SITE_DESC = 'arabic vod'
+ 
+URL_MAIN = 'http://aracinema.co/'
+
+
+MOVIE_EN = ('http://aracinema.co/category/movies/', 'showMovies')
+SERIE_NEWS = ('http://aracinema.co/category/airing//', 'showTvshows')
+SERIE_EN = ('http://aracinema.co/stat/ongoing/', 'showTvshows')
+SERIE_ASIA = ('http://aradrama.net/category/series/%D8%A7%D9%84%D8%AF%D8%B1%D8%A7%D9%85%D8%A7-%D8%A7%D9%84%D8%AA%D8%A7%D9%8A%D9%88%D8%A7%D9%86%D9%8A%D8%A9-%D9%88%D8%A7%D9%84%D8%B5%D9%8A%D9%86%D9%8A%D8%A9/', 'showTvshows')
+
+
+URL_SEARCH = ('http://aracinema.co/?s=', 'showMovies')
+FUNCTION_SEARCH = 'showMovies'
+ 
+def load():
+    oGui = cGui()
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', 'http://venom/')
+    oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler)
+
+            
+    oGui.setEndOfDirectory()
+ 
+def showSearch():
+    oGui = cGui()
+ 
+    sSearchText = oGui.showKeyBoard()
+    if (sSearchText != False):
+        sUrl = 'http://aracinema.co/?s='+sSearchText
+        showMovies(sUrl)
+        oGui.setEndOfDirectory()
+        return
+   
+
+ 
+def showMovies(sSearch = ''):
+    oGui = cGui()
+    if sSearch:
+      sUrl = sSearch
+    else:
+        oInputParameterHandler = cInputParameterHandler()
+        sUrl = oInputParameterHandler.getValue('siteUrl')
+ 
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+ 
+
+    # 'src="([^<]+)" class=".+?href="([^<]+)">([^<]+)</.+?<div class="movieDesc">([^<]+)</div>'
+    sPattern ='<a class="first_A" href="([^<]+)" title="([^<]+)"><img src="([^<]+)" alt='
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+	
+	
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+ 
+            sTitle = aEntry[1]
+            siteUrl = str(aEntry[0])
+
+
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl',siteUrl)
+            oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[1]))
+            oOutputParameterHandler.addParameter('sThumbnail', str(aEntry[2]))
+			
+
+            oGui.addTV(SITE_IDENTIFIER, 'showMoviesLinks', aEntry[1], '', aEntry[2], '', oOutputParameterHandler)
+        
+        cConfig().finishDialog(dialog)
+ 
+        sNextPage = __checkForNextPage(sHtmlContent)
+        if (sNextPage != False):
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sNextPage)
+            oGui.addDir(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', 'next.png', oOutputParameterHandler)
+ 
+    if not sSearch:
+        oGui.setEndOfDirectory()
+		
+def showTvshows(sSearch = ''):
+    oGui = cGui()
+    if sSearch:
+      sUrl = sSearch
+    else:
+        oInputParameterHandler = cInputParameterHandler()
+        sUrl = oInputParameterHandler.getValue('siteUrl')
+ 
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+ 
+
+    # 'src="([^<]+)" class=".+?href="([^<]+)">([^<]+)</.+?<div class="movieDesc">([^<]+)</div>'
+    sPattern ='class="first_A" href="([^<]+)" title="([^<]+)"><img src="([^<]+)" alt.+?<a href="([^<]+)" title="حالة العمل الفني"><i class="icon-feed mi">'
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+	
+	
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+ 
+            sTitle = aEntry[1]
+            siteUrl = str(aEntry[0])
+            sInfo = str(aEntry[3])
+
+
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl',siteUrl)
+            oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[1]))
+            oOutputParameterHandler.addParameter('sThumbnail', str(aEntry[2]))
+			
+            if 'stat' in sInfo:
+                oGui.addMisc(SITE_IDENTIFIER, 'showSeason', aEntry[1], '', aEntry[2], '', oOutputParameterHandler)
+        
+        cConfig().finishDialog(dialog)
+ 
+        sNextPage = __checkForNextPage(sHtmlContent)
+        if (sNextPage != False):
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sNextPage)
+            oGui.addDir(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', 'next.png', oOutputParameterHandler)
+ 
+    if not sSearch:
+        oGui.setEndOfDirectory() 
+		
+def showMoviesLinks():
+    
+    oGui = cGui()
+    
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
+ 
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+
+    #print sUrl
+   
+    oParser = cParser()
+    
+    #Recuperation infos
+    sNote = ''
+    sCom = ''
+
+    sPattern = '<span>تقييم الفيلم : </span>(.+?)</p><h3>القصة :</h3><p>(.+?)</p></div>'
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    
+    if (aResult[0]):
+        sNote = aResult[1][0][0]
+        sCom = aResult[1][0][1]
+        sCom = cUtil().removeHtmlTags(sCom)
+    if (sNote):
+        oGui.addText(SITE_IDENTIFIER,'تقييم الفيلم : ' + str(sNote))
+
+    
+    
+    #Affichage du menu  
+    oGui.addText(SITE_IDENTIFIER,'[COLOR olive][/COLOR]')
+
+
+
+    #on regarde si dispo dans d'autres qualités
+    sPattern = '<a class="wpb_button_a" title="(.+?)" href="(.+?)"><span class="wpb_button  wpb_btn-warning wpb_btn-large">'
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+
+            sTitle = sMovieTitle +  ' - [COLOR skyblue]' + aEntry[0]+'[/COLOR]'
+            sUrl= aEntry[1].replace('http://aracinema.co/?cat=397&s=','')
+            sUrl2= 'http://aracinema.co/'+sUrl
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl',  sUrl2)
+            oOutputParameterHandler.addParameter('sMovieTitle', str(sMovieTitle))
+            oOutputParameterHandler.addParameter('sThumbnail', str(sThumbnail))
+            oGui.addMovie(SITE_IDENTIFIER, 'showHosters', sTitle, '', sThumbnail, sCom, oOutputParameterHandler)             
+    
+        cConfig().finishDialog(dialog)
+
+    oGui.setEndOfDirectory()
+		
+def showSeason():
+    oGui = cGui()
+    
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
+ 
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+ 
+    sPattern = 'class="wpb_button_a" title="([^<]+)" href="([^<]+)"><span class'
+
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+	
+	
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+ 
+            sTitle = aEntry[0]
+            siteUrl = str(aEntry[1])
+			
+
+
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl',siteUrl)
+            oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[0]))
+            oOutputParameterHandler.addParameter('sThumbnail', sThumbnail)
+            oGui.addMisc(SITE_IDENTIFIER, 'showSeries', aEntry[0], '', sThumbnail, '', oOutputParameterHandler)
+        
+        cConfig().finishDialog(dialog)
+       
+    oGui.setEndOfDirectory()
+	
+def showSeries():
+    oGui = cGui()
+    
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
+ 
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+ 
+    sPattern = 'class="first_A" href="([^<]+)" title="([^<]+)"><img src="([^<]+)" alt'
+
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+	
+	
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+ 
+            sTitle = aEntry[1]
+            siteUrl = str(aEntry[0])
+			
+
+
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl',siteUrl)
+            oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[1]))
+            oOutputParameterHandler.addParameter('sThumbnail', sThumbnail)
+
+            oGui.addMisc(SITE_IDENTIFIER, 'showHosters', aEntry[1], '', sThumbnail, '', oOutputParameterHandler)
+        
+        cConfig().finishDialog(dialog)
+       
+    oGui.setEndOfDirectory()
+ 
+       
+ 
+ 
+ 
+def __checkForNextPage(sHtmlContent):
+    sPattern = '<link rel="next" href="([^<]+)"/>'
+	
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+ 
+    if (aResult[0] == True):
+        #print aResult[1][0]
+        return aResult[1][0]
+
+    return False
+
+def showHosters():
+    oGui = cGui()
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
+    
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request();
+    #sHtmlContent = sHtmlContent.replace('<iframe src="//www.facebook.com/plugins/like.php','').replace('<iframe src="http://www.facebook.com/plugins/likebox.php','')
+               
+        
+    sPattern = 'src="([^<]+)" allowfullscreen></iframe>'
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+	
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+            
+            url = str(aEntry)
+            if url.startswith('//'):
+                url = 'http:' + url
+            
+            sHosterUrl = url
+            oHoster = cHosterGui().checkHoster(sHosterUrl)
+            if (oHoster != False):
+                oHoster.setDisplayName(sMovieTitle)
+                oHoster.setFileName(sMovieTitle)
+                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail)
+				
+
+        cConfig().finishDialog(dialog) 
+                
+    oGui.setEndOfDirectory()
