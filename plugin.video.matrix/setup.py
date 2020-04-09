4ceb71bc7d45805504ddcf3ebbbe35ce3574871b
zombiB <zombivert@gmail.com>
2020-04-10 00:30:10 +0200

    Update setup.py

--- plugin.video.matrix/setup.py
+++ plugin.video.matrix/setup.py
@@ -1,17 +1,17 @@
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup (
   name = 'matrix',
-  version = '1.0',
+  version = '0.1',
   packages = find_packages(),
 
   # Declare your packages' dependencies here, for eg:
-  install_requires=['foo>=3'],
+  install_requires = ['foo>=3'],
 
   # Fill in these to make your Egg ready for upload to
   # PyPI
-  author = 'Zombi',
+  author = 'Venom',
   author_email = '',
 
   summary = 'Just another Python package for the cheese shop',
@@ -20,6 +20,4 @@ setup (
   long_description= 'Long description of the package',
 
   # could also include long_description, download_url, classifiers, etc.
-
-  
 )
