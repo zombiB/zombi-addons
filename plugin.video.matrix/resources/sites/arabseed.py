03f05cd7b426aaeef4fbb6b99b3e02af803222eb
zombiB <zombivert@gmail.com>
2020-04-24 08:05:16 +0100

    arabseed RAMADAN_SERIES

--- plugin.video.matrix/resources/sites/arabseed.py
+++ plugin.video.matrix/resources/sites/arabseed.py
@@ -33,6 +33,7 @@ SERIE_ASIA = ('https://arabseed.net/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A
 SERIE_EN = ('https://arabseed.net/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A', 'showSeries')
 SERIE_AR = ('https://arabseed.net/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B9%D8%B1%D8%A8%D9%8A%D9%87', 'showSeries')
 
+RAMADAN_SERIES = ('https://arabseed.net/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B1%D9%85%D8%B6%D8%A7%D9%86-2020', 'showSeries')
 ANIM_NEWS = ('https://arabseed.net/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D9%83%D8%B1%D8%AA%D9%88%D9%86', 'showSeries')
 
 REPLAYTV_NEWS = ('https://arabseed.net/category/%D8%A8%D8%B1%D8%A7%D9%85%D8%AC-%D8%AA%D9%84%D9%81%D8%B2%D9%8A%D9%88%D9%86%D9%8A%D8%A9', 'showMovies')
