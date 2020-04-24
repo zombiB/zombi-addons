4ff680c8802d6d5c065ebeb5cf7cb5f560a96d10
zombiB <zombivert@gmail.com>
2020-04-24 08:04:20 +0100

    cimaclub up

--- plugin.video.matrix/resources/sites/cimaclub.py
+++ plugin.video.matrix/resources/sites/cimaclub.py
@@ -27,7 +27,6 @@ MOVIE_HI = ('http://cimaclub.com/category/%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D9%87%
 MOVIE_ASIAN = ('http://cimaclub.com/category/%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%A7%D8%B3%D9%8A%D9%88%D9%8A%D8%A9/', 'showMovies')
 KID_MOVIES = ('http://cimaclub.com/category/%D8%A7%D9%86%D9%8A%D9%85%D9%8A%D8%B4%D9%86/%D8%A7%D9%81%D9%84%D8%A7%D9%85-%D8%A7%D9%86%D9%8A%D9%85%D9%8A%D8%B4%D9%86/', 'showMovies')
 
-RAMADAN_SERIES = ('http://cimaclub.com/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%B9%D8%B1%D8%A8%D9%8A/%D8%B1%D9%85%D8%B6%D8%A7%D9%86-2019/', 'showSerie')
 
 SERIE_TR = ('http://cimaclub.com/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%AA%D8%B1%D9%83%D9%8A%D8%A9/', 'showSerie')
 SERIE_EN = ('http://cimaclub.com/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA-%D8%A7%D8%AC%D9%86%D8%A8%D9%8A/', 'showSerie')
