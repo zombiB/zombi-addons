a7f982b87b1a3190051aabd12def78f28d85ba60
fagmixy <35014407+fagmixy@users.noreply.github.com>
2022-01-02 21:04:34 +0100

    add section أهداف و ملخصات و بث مباشر

--- plugin.video.matrix/resources/sites/yallalive.py
+++ plugin.video.matrix/resources/sites/yallalive.py
@@ -23,7 +23,7 @@ def load():
     
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', SPORT_FOOT[0])
-    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'رياضة', 'sport.png', oOutputParameterHandler)
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أهداف و ملخصات و بث مباشر', 'sport.png', oOutputParameterHandler)
     
     oGui.setEndOfDirectory()
 
