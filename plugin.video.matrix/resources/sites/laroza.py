a6b9b3202c674edc3b75f7a6d3793aed9cda2512
Mr-7mdan <Mr-Hamdan@hotmail.com>
2023-07-17 18:31:42 +0300

    Fixes
    - Fixed Hosters Function for movies

--- plugin.video.matrix/resources/sites/laroza.py
+++ plugin.video.matrix/resources/sites/laroza.py
@@ -26,12 +26,18 @@ URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
 
 MOVIE_AR = (URL_MAIN + 'category.php?cat=arabic-movies10', 'showMovies')
 MOVIE_EN = (URL_MAIN + 'category.php?cat=english-movies2', 'showMovies')
+MOVIE_HE = (URL_MAIN + 'category03?cat=indian-movies3', 'showMovies')
+MOVIE_TR = (URL_MAIN + '/category.php?cat=aflam3isk', 'showMovies')
+MOVIE_DUBBED = (URL_MAIN + 'category.php?cat=aflammdblgh', 'showMovies')
+MOVIE_ANIME = (URL_MAIN + 'category.php?cat=anime-movies', 'showMovies')
+MOVIE_ASIAN = (URL_MAIN + 'category.php?cat=asian-movies', 'showMovies')
 RAMADAN_SERIES = (URL_MAIN + 'category.php?cat=ramadan-2023', 'showSeries')
 SERIE_EN = (URL_MAIN + 'category.php?cat=english-series3', 'showSeries')
 SERIE_AR = (URL_MAIN + 'category.php?cat=arabic-series22', 'showSeries')
 SERIE_HEND = (URL_MAIN + 'category.php?cat=4indian-series', 'showSeries')
 SERIE_TR = (URL_MAIN + 'category.php?cat=turkish-3isk-seriess21', 'showSeries')
-
+PROGRAMS = (URL_MAIN + 'category.php?cat=tv-programs5', 'showMovies')
+THEATER = (URL_MAIN + '/category.php?cat=masrh1', 'showMovies')
 URL_SEARCH = (URL_MAIN + '/search.php?keywords=', 'showSeries')
 URL_SEARCH_MOVIES = (URL_MAIN + '/search.php?keywords=', 'showMovies')
 URL_SEARCH_SERIES = (URL_MAIN + '/search.php?keywords=', 'showSeries')
@@ -48,12 +54,27 @@ def load():
     oGui.addDir(SITE_IDENTIFIER, 'showSeriesSearch', 'SEARCH_SERIES', icons + '/Search.png', oOutputParameterHandler)
 
     oOutputParameterHandler.addParameter('siteUrl', RAMADAN_SERIES[0])
-    oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'رمضان', icons + '/Ramadan.png', oOutputParameterHandler)
+    oGui.addDir(SITE_IDENTIFIER, 'showRamdan', 'رمضان', icons + '/Ramadan.png', oOutputParameterHandler)
 
     oOutputParameterHandler.addParameter('siteUrl', MOVIE_EN[0])
     oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أفلام أجنبية',icons + '/MoviesEnglish.png', oOutputParameterHandler)
    
     oOutputParameterHandler.addParameter('siteUrl', MOVIE_AR[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أفلام عربية',icons + '/Arabic.png', oOutputParameterHandler)
+
+    # oOutputParameterHandler.addParameter('siteUrl', MOVIE_TR[0])
+    # oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أفلام تركية',icons + '/Turkish.png', oOutputParameterHandler)
+
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_TR[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أفلام آسيوية',icons + '/Asian.png', oOutputParameterHandler)
+
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_DUBBED[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أفلام مدبلجة',icons + '/Dubbed.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_ANIME[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أفلام انمي',icons + '/Anime.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_HE[0])
     oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'أفلام عربية',icons + '/Arabic.png', oOutputParameterHandler)
 	
     oOutputParameterHandler.addParameter('siteUrl', SERIE_AR[0])
@@ -68,9 +89,13 @@ def load():
     oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'مسلسلات هندية',icons + '/Hindi.png', oOutputParameterHandler)
 
     oOutputParameterHandler = cOutputParameterHandler()
-    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + 'category.php?cat=8rmdan-2022')
-    oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'رمضان 2022', icons + '/Ramadan.png', oOutputParameterHandler)
+    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + THEATER[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'مسرحيات', icons + '/Theater.png', oOutputParameterHandler)
 
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + PROGRAMS[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'برامج تلفزيونية', icons + '/Programs.png', oOutputParameterHandler)
+    
     oGui.setEndOfDirectory()
  
 def showSeriesSearch():
@@ -92,7 +117,21 @@ def showSearch():
         showMovies(sUrl)
         oGui.setEndOfDirectory()
         return
-	
+
+def showRamdan():
+    oGui = cGui()
+    List= []
+    List.append(['Ramadan 2023', 'category.php?cat=ramadan-2023', 'showSeries'])
+    List.append(['Ramadan 2022','category.php?cat=26ramadan-2022', 'showSeries'])
+    List.append(['Ramadan 2021','category.php?cat=18ramadan-2021', 'showSeries'])
+    List.append(['Ramadan 2020','category.php?cat=2ramadan-2020', 'showSeries'])
+    
+    for li in List:
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + li[1])
+        oGui.addDir(SITE_IDENTIFIER, li[2], li[0], icons + '/Ramadan.png', oOutputParameterHandler)
+    oGui.setEndOfDirectory()
+    
 def showMovies(sSearch = ''):
     oGui = cGui()
     if sSearch:
@@ -130,7 +169,8 @@ def showMovies(sSearch = ''):
             sTitle = aEntry[1].replace("مشاهدة","").replace("مشاهده","").replace("مترجم","").replace("فيلم","").replace("اونلاين","").replace("اون لاين","").replace("برنامج","").replace("WEB-DL","").replace("BRRip","").replace("720p","").replace("HD-TC","").replace("HDRip","").replace("HD-CAM","").replace("DVDRip","").replace("BluRay","").replace("1080p","").replace("WEBRip","").replace("WEB-dl","").replace("4K","").replace("All","").replace("BDRip","").replace("HDCAM","").replace("HDTC","").replace("HDTV","").replace("HD","").replace("720","").replace("HDCam","").replace("Full HD","").replace("1080","").replace("HC","").replace("Web-dl","").replace("انمي","")
  
  
-            siteUrl = aEntry[0].replace("watch.php","play.php")
+            Url = aEntry[0]
+            siteUrl = aEntry[0].replace("watch.php","play.php").replace("video.php","play.php") + '|Referer=' + Url
             sDesc = ""
             sThumb = aEntry[2]
             sYear = ''
@@ -298,13 +338,15 @@ def showHosters():
     sUrl = oInputParameterHandler.getValue('siteUrl')
     sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
     sThumb = oInputParameterHandler.getValue('sThumb')
+   
 
 
-
-    oRequestHandler = cRequestHandler(sUrl)
+    oRequestHandler = cRequestHandler(sUrl.split("|Referer=")[0])
     oRequestHandler.addHeaderEntry('User-Agent', UA)
+    oRequestHandler.addHeaderEntry('Referer', sUrl.split("|Referer=")[1])
+    
     sHtmlContent = oRequestHandler.request()
-
+    
     # ([^<]+) .+?
                
 
@@ -317,6 +359,8 @@ def showHosters():
         for aEntry in aResult[1]:
             
             sHosterUrl = aEntry
+            VSlog(sHosterUrl)
+            
             sTitle =  ""
             if sHosterUrl.startswith('//'):
                 sHosterUrl = 'http:' + sHosterUrl
