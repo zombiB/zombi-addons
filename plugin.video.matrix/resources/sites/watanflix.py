0489a913380a687f80df71c5e3a1f3bcf18d8db1
Mr-7mdan <Mr-Hamdan@hotmail.com>
2023-07-18 18:26:57 +0300

    Add files via upload
    Added more site categories

--- plugin.video.matrix/resources/sites/watanflix.py
+++ plugin.video.matrix/resources/sites/watanflix.py
@@ -8,7 +8,7 @@ from resources.lib.gui.gui import cGui
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
-from resources.lib.comaddon import progress, siteManager, addon
+from resources.lib.comaddon import progress, siteManager, addon, VSlog
 from resources.lib.parser import cParser
 
 ADDON = addon()
@@ -23,6 +23,11 @@ URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
 RAMADAN_SERIES = (URL_MAIN + '/ar/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA', 'showSeries')
 SERIE_AR = (URL_MAIN + '/ar/category/%D9%85%D8%B3%D9%84%D8%B3%D9%84%D8%A7%D8%AA', 'showSeries')
 KID_CARTOON = (URL_MAIN + '/ar/category/%D8%A3%D8%B7%D9%81%D8%A7%D9%84', 'showSerie')
+MOVIES = (URL_MAIN + '/category/%D8%A7%D9%84%D8%A3%D9%81%D9%84%D8%A7%D9%85','showSerie')
+THEATER = (URL_MAIN + '/category/%D9%85%D8%B3%D8%B1%D8%AD%D9%8A%D8%A7%D8%AA','showSerie')
+PROGRAMS = (URL_MAIN + '/category/%D8%A8%D8%B1%D8%A7%D9%85%D8%AC','showSerie')
+
+
 
 SERIE_GENRES = (True, 'showGenres')
 
@@ -37,14 +42,27 @@ def load():
     oOutputParameterHandler.addParameter('siteUrl', 'http://venom/')
     oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Search', icons + '/Search.png', oOutputParameterHandler)
 
+    oOutputParameterHandler.addParameter('siteUrl', SERIE_GENRES[0])
+    oGui.addDir(SITE_IDENTIFIER, SERIE_GENRES[1], 'قسم المسلسلات', icons + '/TVShows.png', oOutputParameterHandler)
+    
     oOutputParameterHandler.addParameter('siteUrl', SERIE_AR[0])
     oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'مسلسلات عربية', icons + '/Arabic.png', oOutputParameterHandler)
  
     oOutputParameterHandler.addParameter('siteUrl', KID_CARTOON[0])
     oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'مسلسلات كرتون', icons + '/Cartoon.png', oOutputParameterHandler) 
+
+    oOutputParameterHandler.addParameter('siteUrl', MOVIES[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'أفلام عربية', icons + '/Movies.png', oOutputParameterHandler)
+
+    oOutputParameterHandler.addParameter('siteUrl', THEATER[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'مسرحيات', icons + '/Theater.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler.addParameter('siteUrl', PROGRAMS[0])
+    oGui.addDir(SITE_IDENTIFIER, 'showSeries', 'برامج', icons + '/Movies.png', oOutputParameterHandler)
     
     oGui.setEndOfDirectory()
- 
+     
+    
 def showSearch():
     oGui = cGui()
  
@@ -86,7 +104,7 @@ def showSeriesSearch(sSearch = ''):
  
             sTitle = aEntry[0]
             siteUrl = aEntry[1]
-            sThumb = ""
+            sThumb = ''
             sYear = ""
             sDesc = ""
 
@@ -120,8 +138,16 @@ def showGenres():
     liste.append( ["كوميدي","https://watanflix.com/ar/type/%D9%83%D9%88%D9%85%D9%8A%D8%AF%D9%8A"] )
     liste.append( ["دراما","https://watanflix.com/ar/type/%D8%AF%D8%B1%D8%A7%D9%85%D8%A7"] )
     liste.append( ["حارة-شامية","https://watanflix.com/ar/type/%D8%AD%D8%A7%D8%B1%D8%A9-%D8%B4%D8%A7%D9%85%D9%8A%D8%A9"] )
-    liste.append( ["تاريخي-سيرة-ذاتيه-وثائقي","https://watanflix.com/ar/type/%D8%AA%D8%A7%D8%B1%D9%8A%D8%AE%D9%8A-%D8%B3%D9%8A%D8%B1%D8%A9-%D8%B0%D8%A7%D8%AA%D9%8A%D9%87-%D9%88%D8%AB%D8%A7%D8%A6%D9%82%D9%8A"] )
+    liste.append( ["تاريخي وثائقي","https://watanflix.com/ar/type/%D8%AA%D8%A7%D8%B1%D9%8A%D8%AE%D9%8A-%D8%B3%D9%8A%D8%B1%D8%A9-%D8%B0%D8%A7%D8%AA%D9%8A%D9%87-%D9%88%D8%AB%D8%A7%D8%A6%D9%82%D9%8A"] )
     liste.append( ["غموض-تشويق","https://watanflix.com/ar/type/%D8%BA%D9%85%D9%88%D8%B6-%D8%AA%D8%B4%D9%88%D9%8A%D9%82"] )
+    liste.append( ["رومنسي","https://watanflix.com/ar/type/%D8%B1%D9%88%D9%85%D9%86%D8%B3%D9%8A"] )
+    liste.append( ["اجتماعي","https://watanflix.com/ar/type/%D8%A7%D8%AC%D8%AA%D9%85%D8%A7%D8%B9%D9%8A"] )
+    liste.append( ["رعب","https://watanflix.com/ar/type/%D8%B1%D8%B9%D8%A8"] )
+    liste.append( ["اكشن","https://watanflix.com/ar/type/%D8%A3%D9%83%D8%B4%D9%86"] )
+    liste.append( ["رياضي","https://watanflix.com/ar/type/%D8%B1%D9%8A%D8%A7%D8%B6%D9%8A"] )
+    liste.append( ["مقابلا تلفزيونية","https://watanflix.com/ar/type/%D9%85%D9%82%D8%A7%D8%A8%D9%84%D8%A7%D8%AA-%D8%AA%D9%84%D9%81%D8%B2%D9%8A%D9%88%D9%86%D9%8A%D8%A9"] )
+    liste.append( ["ديني","https://watanflix.com/ar/type/%D8%AF%D9%8A%D9%86%D9%8A"] )
+    liste.append( ["طبخ","https://watanflix.com/ar/type/%D8%B7%D8%A8%D8%AE"] )
     
     for sTitle,sUrl in liste:
         
