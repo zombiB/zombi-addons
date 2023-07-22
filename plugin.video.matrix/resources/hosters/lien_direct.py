dd9dc54c3275801f43f24bff278f3a5c54961382
Mr-7mdan <Mr-Hamdan@hotmail.com>
2023-07-22 03:02:12 +0300

    Update lien_direct.py

--- plugin.video.matrix/resources/hosters/lien_direct.py
+++ plugin.video.matrix/resources/hosters/lien_direct.py
@@ -11,7 +11,7 @@ UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:68.0) Gecko/20100101 Firefox/6
 class cHoster(iHoster):
 
     def __init__(self):
-        iHoster.__init__(self, 'lien_direct', 'Lien direct')
+        iHoster.__init__(self, 'lien_direct', 'Direct Link')
 
     def setUrl(self, url):
         self._url = str(url).replace('+', '%20') # un lien direct n'est pas forcement urlEncoded
